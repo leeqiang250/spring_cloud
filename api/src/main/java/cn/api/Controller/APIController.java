8848a04aa2725006a7b54dd1192c1e3dfe44a194
leeqiang250 <leeqiang250@163.com>
2019-08-15 16:22:17 +0800

    BUG修复。

--- api/src/main/java/cn/api/Controller/APIController.java
+++ api/src/main/java/cn/api/Controller/APIController.java
@@ -2,12 +2,16 @@ package cn.api.Controller;
 
 import cn.DTO;
 import cn.Model;
+import cn.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/v0/api")
 public class APIController {
 
-    public void dffds() {
-        DTO dto = new DTO();
-        Model model = new Model();
-
+    public Response<Boolean> get() {
+        return Response.Success();
     }
+
 }

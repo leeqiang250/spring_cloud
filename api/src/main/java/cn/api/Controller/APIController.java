915f238c56dc5571dbbb0cc619d2558c2f1827d7
leeqiang250 <leeqiang250@163.com>
2019-08-15 16:11:28 +0800

    BUG修复。

---
+++ api/src/main/java/cn/api/Controller/APIController.java
@@ -0,0 +1,13 @@
+package cn.api.Controller;
+
+import cn.DTO;
+import cn.Model;
+
+public class APIController {
+
+    public void dffds() {
+        DTO dto = new DTO();
+        Model model = new Model();
+
+    }
+}

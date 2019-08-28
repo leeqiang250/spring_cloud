e9d09cde08fabebbf842f1edcc183bed727bc718
leeqiang250 <leeqiang250@163.com>
2019-08-28 17:33:24 +0800

    BUG修复。

--- resource/launch.sh
+++ resource/launch.sh
@@ -1,13 +1,16 @@
 cd /Users/liqiang/Documents/SourceTree/spring_cloud/eurekaserver
 mvn clean install
-cd /Users/liqiang/Documents/SourceTree/spring_cloud/user
-mvn clean install
 cd /Users/liqiang/Documents/SourceTree/spring_cloud/api
+mvn clean install
+cd /Users/liqiang/Documents/SourceTree/spring_cloud/config
+mvn clean install
+cd /Users/liqiang/Documents/SourceTree/spring_cloud/user
 mvn clean install
 cd /Users/liqiang/Documents/SourceTree/spring_cloud/manager
 mvn clean install
 
 java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/eurekaserver/target/eurekaserver-1.0-SNAPSHOT.jar
-java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/api/target/api-1.0-SNAPSHOT
+java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/api/target/api-1.0-SNAPSHOT.jar
+java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/config/target/config-1.0-SNAPSHOT.jar
 java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/user/target/user-1.0-SNAPSHOT.jar
 java -jar /Users/liqiang/Documents/SourceTree/spring_cloud/manager/target/manager-1.0-SNAPSHOT.jar
